2f214ae51b4aa04ccc2cb55169170191beea4f4a
tiagoomag <tiagosantosmagalhaes@hotmail.com>
2024-05-16 23:41:13 -0300

    Initial commit

---
+++ src/main/java/br/com/tm/BarApplication.java
@@ -0,0 +1,13 @@
+package br.com.tm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarApplication.class, args);
+	}
+
+}
